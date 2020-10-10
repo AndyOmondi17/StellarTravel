b32165b15ff764bb1b349b9478f75ef4405873a2
AndyOmondi17 <andyomondi17@gmail.com>
2020-10-10 16:42:25 +0300

    Create website layout

---
+++ js/scripts.js
@@ -0,0 +1,149 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+<script >/* LOGIN - MAIN.JS - dp 2017 */
+
+// LOGIN TABS
+$(function() {
+	var tab = $('.tabs h3 a');
+	tab.on('click', function(event) {
+		event.preventDefault();
+		tab.removeClass('active');
+		$(this).addClass('active');
+		tab_content = $(this).attr('href');
+		$('div[id$="tab-content"]').removeClass('active');
+		$(tab_content).addClass('active');
+	});
+});
+
+// SLIDESHOW
+$(function() {
+	$('#slideshow > div:gt(0)').hide();
+	setInterval(function() {
+		$('#slideshow > div:first')
+		.fadeOut(1000)
+		.next()
+		.fadeIn(1000)
+		.end()
+		.appendTo('#slideshow');
+	}, 3850);
+});
+
+// CUSTOM JQUERY FUNCTION FOR SWAPPING CLASSES
+(function($) {
+	'use strict';
+	$.fn.swapClass = function(remove, add) {
+		this.removeClass(remove).addClass(add);
+		return this;
+	};
+}(jQuery));
+
+// SHOW/HIDE PANEL ROUTINE (needs better methods)
+// I'll optimize when time permits.
+$(function() {
+	$('.agree,.forgot, #toggle-terms, .log-in, .sign-up').on('click', function(event) {
+		event.preventDefault();
+		var terms = $('.terms'),
+        recovery = $('.recovery'),
+        close = $('#toggle-terms'),
+        arrow = $('.tabs-content .fa');
+		if ($(this).hasClass('agree') || $(this).hasClass('log-in') || ($(this).is('#toggle-terms')) && terms.hasClass('open')) {
+			if (terms.hasClass('open')) {
+				terms.swapClass('open', 'closed');
+				close.swapClass('open', 'closed');
+				arrow.swapClass('active', 'inactive');
+			} else {
+				if ($(this).hasClass('log-in')) {
+					return;
+				}
+				terms.swapClass('closed', 'open').scrollTop(0);
+				close.swapClass('closed', 'open');
+				arrow.swapClass('inactive', 'active');
+			}
+		}
+		else if ($(this).hasClass('forgot') || $(this).hasClass('sign-up') || $(this).is('#toggle-terms')) {
+			if (recovery.hasClass('open')) {
+				recovery.swapClass('open', 'closed');
+				close.swapClass('open', 'closed');
+				arrow.swapClass('active', 'inactive');
+			} else {
+				if ($(this).hasClass('sign-up')) {
+					return;
+				}
+				recovery.swapClass('closed', 'open');
+				close.swapClass('closed', 'open');
+				arrow.swapClass('inactive', 'active');
+			}
+		}
+	});
+});
+
+// DISPLAY MSSG
+$(function() {
+	$('.recovery .button').on('click', function(event) {
+		event.preventDefault();
+		$('.recovery .mssg').addClass('animate');
+		setTimeout(function() {
+			$('.recovery').swapClass('open', 'closed');
+			$('#toggle-terms').swapClass('open', 'closed');
+			$('.tabs-content .fa').swapClass('active', 'inactive');
+			$('.recovery .mssg').removeClass('animate');
+		}, 2500);
+	});
+});
+
+// DISABLE SUBMIT FOR DEMO
+$(function() {
+	$('.button').on('click', function(event) {
+		$(this).stop();
+		event.preventDefault();
+		return false;
+	});
+});
+//# sourceURL=pen.js
+
+</script>
+$(document).ready(function(){
+  $("#imageDetails1").click(function(){
+      $("#imageDetails1").toggle();
+      $("#developerdetails1").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#img1").click(function(){
+    $("#para1").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#img2").click(function(){
+    $("#para2").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#img3").click(function(){
+    $("#para3").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#img4").click(function(){
+    $("#para4").toggle();
+  });
+});
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var name=$("input#name").val();
+  alert("Hello "+name+" Thank you for contacting us");
+  event.preventDefault();
+  });
+});
